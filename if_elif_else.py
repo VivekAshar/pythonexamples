53cd9f072ecbba017dc0c9b96a826144f44de5d1
Srinivas Krishnamurthy <ksrinivas0575@gmail.com>
2021-04-21 22:34:26 +0530

    Initial commit

---
+++ if_elif_else.py
@@ -0,0 +1,62 @@
+'''
+Examples are used to show usage of
+if-elif-else loop
+'''
+
+'''
+# if usage
+var1 = 25
+if (var1 > 15):
+   print("I am inside if loop")
+   print("var1 is less than 15")
+   print("var1=",var1)
+print ("I am outside if loop")
+'''
+
+'''
+# if-else usage
+a = 4
+b = 4
+
+if a<b:
+	print(a, 'is less than', b)
+else:
+	print(a, 'is not less than', b)
+'''
+
+'''
+var1 = 0 # var1 = 100, 0, -6
+if var1:
+   print ("1 - Got a true expression value")
+   print ("var1=",var1)
+else:
+   print ("1 - Got a false expression value")
+   print ("var1=",var1)
+
+var2 = -6 # var1 = 0, 10, 100
+if var2:
+   print ("2 - Got a true expression value")
+   print ("var1=",var2)
+else:
+   print ("2 - Got a false expression value")
+   print ("var1=",var2)
+
+print ("Done")
+'''
+
+'''
+var = 500 #200 150 500 100
+if var == 200:
+   print ("1 - Got a true expression value")
+   print (var)
+elif var == 150:
+   print ("2 - Got a true expression value")
+   print (var)
+elif var == 100:
+   print ("3 - Got a true expression value")
+   print (var)
+else:
+   print ("4 - Got a false expression value")
+   print (var)
+   
+'''
