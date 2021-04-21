953785bcd58afe421c98fcab175e2c0a3ff1eed8
Srinivas Krishnamurthy <ksrinivas0575@gmail.com>
2021-04-21 23:20:52 +0530

    Updated the examples

---
+++ if_elif_else/example06.py
@@ -0,0 +1,10 @@
+var1 = 100 #100 5
+if (var1 == 10):
+   if (var1 < 15):
+        print ("var1 is smaller than 15")
+   if (var1 < 12):
+        print ("var1 is smaller than 12 too")
+   else:
+        print ("var1 is greater than 15")
+else:
+    print("var1 is not Equal to 10")
