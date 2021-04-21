13600e6897b4e512881da0b4796d8aec052b1978
srinivas0575 <67730432+srinivas0575@users.noreply.github.com>
2021-04-21 23:27:07 +0530

    created unsloved01.py
    created unsloved01.py

---
+++ if_elif_else/Unsolved01.py
@@ -0,0 +1,6 @@
+Wtite a python program to check the user input abbreviation.  
+If the user enters "lol", print "laughing out loud".
+If the user enters "rofl", print "rolling on the floor laughing".
+If the user enters "lmk", print "let me know".
+If the user enters "smh", print "shaking my head".
+
