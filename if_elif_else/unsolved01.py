18031ff463ced16ee5def4c422c4d7b4c0e820df
Srinivas Krishnamurthy <ksrinivas0575@gmail.com>
2021-04-21 23:35:59 +0530

    added 1 more unsolved

--- if_elif_else/unsolved01.py
+++ if_elif_else/unsolved01.py
@@ -1,4 +1,4 @@
-Wtite a python program to check the user input abbreviation.  
+Write a python program to check the user input abbreviation.
 If the user enters "lol", print "laughing out loud".
 If the user enters "rofl", print "rolling on the floor laughing".
 If the user enters "lmk", print "let me know".
