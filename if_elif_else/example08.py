5263bccef10dc2a552714ba073ff181b556ffc6c
Srinivas Krishnamurthy <ksrinivas0575@gmail.com>
2021-04-21 23:52:10 +0530

    added 2 more examples

---
+++ if_elif_else/example08.py
@@ -0,0 +1,6 @@
+var1 = input("Enter a number greater than 10 or less than 20: ")
+var1 = int(var1)
+if (var1 > 10 or var1 < 20):
+    print("Entered value is in the range")
+else:
+    print("Please try again!")
