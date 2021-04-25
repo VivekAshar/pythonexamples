4271a36e84eed7dc4abb087be31b06d58df2f6aa
Srinivas Krishnamurthy <ksrinivas0575@gmail.com>
2021-04-26 00:09:25 +0530

    added 1 more examples

---
+++ Basics/example02.py
@@ -0,0 +1,19 @@
+# This is one line comment
+
+# This is
+# multiple line
+# comments
+
+'''
+This is one more
+multiple line
+comments
+'''
+
+'''
+Program name - examples02.py
+Author - Srinivas
+Project - Python training
+Date - 26th Apr 2021
+Contact - srinivas.k@ltts.com
+'''
