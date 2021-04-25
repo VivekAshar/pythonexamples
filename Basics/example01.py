de0716bc59ce58c2710ed2dd9a9a7637c8b3f07f
Srinivas Krishnamurthy <ksrinivas0575@gmail.com>
2021-04-26 00:02:23 +0530

    added exmaple under basics

---
+++ Basics/example01.py
@@ -0,0 +1,18 @@
+print ('Welcome to python programming')
+
+print ("Welcome to python programming")
+
+x = 5
+print ("x=",x)
+print (type(x))
+print (id(x))
+
+x = 5.5
+print ("x=",x)
+print (type(x))
+print (id(x))
+
+x = "5"
+print ("x=",x)
+print (type(x))
+print (id(x))
