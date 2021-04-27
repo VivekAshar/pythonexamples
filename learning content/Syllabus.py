71500ea47b1d13f88b77e3cd1d1fc2d98967ea68
Srinivas Krishnamurthy <ksrinivas0575@gmail.com>
2021-04-28 01:47:10 +0530

    added 2 more examples

---
+++ learning content/Syllabus.py
@@ -0,0 +1,12 @@
+Level 1
+
+Setting up execution environment
+Basic Syntax and Data Types
+Strings
+Control Structures & Loops
+Data Structures
+Functions and Lambda expressions
+File Handling (I/O)
+Modules and Packages
+Introduction to OS, SYS Modules
+Unit test framework (pytest)
